dadf506a0a9783502b2f292092299256452c13d2
insprac <insprac.dev@gmail.com>
2025-06-07 21:03:00 +1200

    Fleshed out wave spawning system

--- src/waves.rs
+++ src/waves.rs
@@ -2,27 +2,288 @@ use bevy::prelude::*;
 use hexx::Hex;
 
 use crate::{
-    AppState,
+    AppState, GameState,
     arena::Arena,
-    enemy::{EnemySet, SpawnEnemyCommand},
+    enemy::{Enemy, EnemySet, SpawnEnemyCommand},
 };
+
+pub const WAVES: &[Wave] = &[
+    // Wave 1
+    Wave {
+        stages: &[
+            WaveStage {
+                enemies: 3,
+                remaining_threshold: 0,
+            },
+            WaveStage {
+                enemies: 5,
+                remaining_threshold: 1,
+            },
+            WaveStage {
+                enemies: 6,
+                remaining_threshold: 2,
+            },
+        ],
+    },
+
+    // Wave 2
+    Wave {
+        stages: &[
+            WaveStage {
+                enemies: 6,
+                remaining_threshold: 0,
+            },
+            WaveStage {
+                enemies: 8,
+                remaining_threshold: 2,
+            },
+            WaveStage {
+                enemies: 10,
+                remaining_threshold: 4,
+            },
+            WaveStage {
+                enemies: 12,
+                remaining_threshold: 6,
+            },
+        ],
+    },
+
+    // Wave 3
+    Wave {
+        stages: &[
+            WaveStage {
+                enemies: 8,
+                remaining_threshold: 0,
+            },
+            WaveStage {
+                enemies: 10,
+                remaining_threshold: 4,
+            },
+            WaveStage {
+                enemies: 14,
+                remaining_threshold: 6,
+            },
+            WaveStage {
+                enemies: 16,
+                remaining_threshold: 10,
+            },
+        ],
+    },
+
+    // Wave 4
+    Wave {
+        stages: &[
+            WaveStage {
+                enemies: 12,
+                remaining_threshold: 0,
+            },
+            WaveStage {
+                enemies: 16,
+                remaining_threshold: 6,
+            },
+            WaveStage {
+                enemies: 20,
+                remaining_threshold: 8,
+            },
+            WaveStage {
+                enemies: 24,
+                remaining_threshold: 10,
+            },
+            WaveStage {
+                enemies: 26,
+                remaining_threshold: 12,
+            },
+        ],
+    },
+
+    // Wave 5
+    Wave {
+        stages: &[
+            WaveStage {
+                enemies: 16,
+                remaining_threshold: 0,
+            },
+            WaveStage {
+                enemies: 20,
+                remaining_threshold: 12,
+            },
+            WaveStage {
+                enemies: 26,
+                remaining_threshold: 14,
+            },
+            WaveStage {
+                enemies: 30,
+                remaining_threshold: 16,
+            },
+            WaveStage {
+                enemies: 34,
+                remaining_threshold: 18,
+            },
+        ],
+    },
+
+];
+
+pub struct Wave {
+    pub stages: &'static [WaveStage],
+}
+
+pub struct WaveStage {
+    /// How many enemies to spawn, they all spawn at once around the edge of the arena.
+    pub enemies: usize,
+    /// Enemies wont spawn until there is at most this many enemies remaining.
+    pub remaining_threshold: usize,
+}
 
 pub struct WavePlugin;
 
 impl Plugin for WavePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::InGame), spawn_enemies.in_set(EnemySet));
+        app.init_resource::<WaveManager>()
+            .add_event::<WaveStartedEvent>()
+            .add_event::<WaveStageStartedEvent>()
+            .add_systems(OnEnter(AppState::InGame), setup_waves.in_set(EnemySet))
+            .add_systems(
+                Update,
+                (
+                    update_wave_progress,
+                    spawn_stage.run_if(on_event::<WaveStageStartedEvent>),
+                )
+                    .run_if(in_state(AppState::InGame))
+                    .run_if(in_state(GameState::Running)),
+            );
     }
 }
 
-fn spawn_enemies(mut commands: Commands, arena: Res<Arena>) {
+#[derive(Resource)]
+pub struct WaveManager {
+    wave: usize,
+    stage: usize,
+    update_timer: Timer,
+}
+
+impl Default for WaveManager {
+    fn default() -> Self {
+        Self {
+            wave: 0,
+            stage: 0,
+            update_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
+        }
+    }
+}
+
+impl WaveManager {
+    /// The display value of the wave starting from 1.
+    pub fn wave_display(&self) -> usize {
+        self.wave + 1
+    }
+
+    /// The display value of the wave stage starting from 1.
+    pub fn stage_display(&self) -> usize {
+        self.stage + 1
+    }
+}
+
+#[derive(Event)]
+pub struct WaveStartedEvent {
+    /// The wave that just started (1, 2, 3, ...).
+    pub wave: usize,
+}
+
+#[derive(Event)]
+pub struct WaveStageStartedEvent {
+    /// The current wave.
+    pub wave: usize,
+    /// The stage that just started.
+    pub stage: usize,
+}
+
+fn setup_waves(
+    mut wave_manager: ResMut<WaveManager>,
+    mut evw_wave_started: EventWriter<WaveStartedEvent>,
+    mut evw_wave_stage_started: EventWriter<WaveStageStartedEvent>,
+) {
+    wave_manager.wave = 0;
+    wave_manager.stage = 0;
+    evw_wave_started.write(WaveStartedEvent {
+        wave: wave_manager.wave_display(),
+    });
+    evw_wave_stage_started.write(WaveStageStartedEvent {
+        wave: wave_manager.wave_display(),
+        stage: wave_manager.stage_display(),
+    });
+}
+
+fn spawn_stage(mut commands: Commands, arena: Res<Arena>, wave_manager: Res<WaveManager>) {
+    let wave_stage = &WAVES[wave_manager.wave].stages[wave_manager.stage];
+
+    // Get the hexes around the edge of the arena to distribute randomly among them
     let mut hexes: Vec<Hex> = Hex::ZERO.ring(Arena::RADIUS).collect();
-    for _ in 0..20 {
+
+    for _ in 0..wave_stage.enemies {
         if hexes.len() == 0 {
-            break;
+            // Refill hexes and continue
+            hexes = Hex::ZERO.ring(Arena::RADIUS).collect();
         }
         let index = rand::random_range(0..hexes.len() - 1);
         let hex = hexes.swap_remove(index);
         commands.queue(SpawnEnemyCommand::new(arena.layout.hex_to_world_pos(hex)));
+    }
+}
+
+fn update_wave_progress(
+    time: Res<Time>,
+    mut wave_manager: ResMut<WaveManager>,
+    mut evw_wave_started: EventWriter<WaveStartedEvent>,
+    mut evw_wave_stage_started: EventWriter<WaveStageStartedEvent>,
+    q_enemies: Query<Entity, With<Enemy>>,
+) {
+    // Only check for updates once per timer
+    wave_manager.update_timer.tick(time.delta());
+    if !wave_manager.update_timer.just_finished() {
+        return;
+    }
+
+    // Get a total count of enemies
+    let remaining_enemies = q_enemies.iter().len();
+
+    let wave = &WAVES[wave_manager.wave];
+    if wave.stages.len() <= wave_manager.stage + 1 {
+        // This is the last stage
+        if remaining_enemies > 0 {
+            return;
+        }
+        if WAVES.len() <= wave_manager.wave + 1 {
+            // There are no more waves
+            // TODO replace with a victory state
+            info!("Victory!");
+            return;
+        }
+        wave_manager.wave += 1;
+        wave_manager.stage = 0;
+
+        info!(wave=wave_manager.wave_display(), "Wave started");
+
+        evw_wave_started.write(WaveStartedEvent {
+            wave: wave_manager.wave_display(),
+        });
+        evw_wave_stage_started.write(WaveStageStartedEvent {
+            wave: wave_manager.wave_display(),
+            stage: wave_manager.stage_display(),
+        });
+    } else {
+        // There are stages remaining
+        let stage = &wave.stages[wave_manager.stage];
+        if remaining_enemies > stage.remaining_threshold {
+            return;
+        }
+        wave_manager.stage += 1;
+
+        info!(stage=wave_manager.stage_display(), "Stage started");
+
+        evw_wave_stage_started.write(WaveStageStartedEvent {
+            wave: wave_manager.wave_display(),
+            stage: wave_manager.stage_display(),
+        });
     }
 }
